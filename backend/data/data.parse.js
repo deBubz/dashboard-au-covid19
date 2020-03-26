fcb937455c7e89590d0b110852b7f2ae3f8161ce
debubz <bao.es.hoang2@gmail.com>
2020-03-27 04:01:50 +1100

    working, and csv format update

--- backend/data/data.parse.js
+++ backend/data/data.parse.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 // figure this out later
 // const source = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/03-19-2020.csv'
 
-const dataPath = __dirname + '/data.csv'
+const dataPath = __dirname + '/data2.csv'
 const data = fs.readFileSync(dataPath, "utf8");
 
 // get data 
@@ -23,11 +23,11 @@ Papa.parse( data , {
 // parse to array data
 // cut first and last non-data part of the csv
 const array = dataRows.data.slice(1, dataRows.data.length - 1).map( res => {
-    const country = res[1];
-    const state = res[0];
-    const confirmed = res[3];
-    const deaths = res[4];
-    const recovered = res[5];
+    const country = res[3];
+    const state = res[2];
+    const confirmed = res[7];
+    const deaths = res[8];
+    const recovered = res[9];
     return { country, state, confirmed, deaths, recovered }
 })
 
--- src/components/main.component.js
+++ src/components/main.component.js
@@ -2,13 +2,11 @@ import React from 'react';
 import axios from 'axios';
 
 // const Data = props => (
-//     <tr>
-//         {/* <td>{props.d.country}</td>
-//         <td>{props.d.state}</td>
-//         <td>{props.d.w_confirmed}</td>
-//         <td>{props.d.w_recovered}</td>
-//         <td>{props.d.w_deaths}</td> */}
-//     </tr>
+//     <ul>
+//         <li>Confirmed: {props.d.confirmed}</li>
+//         <li>Recovered: {props.d.recovered}</li>
+//         <li>Deaths: {props.d.deaths}</li>
+//     </ul>
 // )
 
 export default class Main extends React.Component {
@@ -23,38 +21,32 @@ export default class Main extends React.Component {
         }
     }
 
-    componentDidMount() {
+    componentWillMount() {
         document.title = "Au Covid Dashboard | Main";
         axios.get('http://localhost:8080/total')
             .then(res => {
                 this.setState({
-                    confirmed: res.data.w_confirmed,
-                    recovered: res.data.w_recovered,
-                    deaths: res.data.w_deaths,
+                    world: {
+                        confirmed: res.data.w_confirmed,
+                        recovered: res.data.w_recovered,
+                        deaths: res.data.w_deaths,
+                    }
                 });
-                console.log(res)
-                console.log(res.data.w_confirmed)
             })
     }
 
     showData() {
         console.log(this.state.world.confirmed);
         return (
+            // <Data d={this.state.world}/>
             <ul>
                 <li>Confirmed: {this.state.world.confirmed}</li>
                 <li>Recovered: {this.state.world.recovered}</li>
-                <li>Deaths: {this.state.world.deaths}</li>
+                <li>Deaths:    {this.state.world.deaths}</li>
             </ul>
-            
         )
     }
-    // showData() {
-    //     // return this.state.world.map(data => {
-    //         console.log(this.state.world.confirmed)
-    //         return <Data d={this.state.world}/>
-    //     // })
-    // }
-
+    
     render() {
         return (
             <div>
